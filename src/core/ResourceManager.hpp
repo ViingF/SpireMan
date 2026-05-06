a1dacda5287dfaca1c93c7e7a3a05f5f2b854008
ViingF <352159817@qq.com>
2026-05-06 15:14:14 +0800

    first commit

--- src/core/ResourceManager.hpp
+++ src/core/ResourceManager.hpp
@@ -13,11 +13,9 @@ public:
     ErrorCode loadTexture(const std::string& id, const std::string& path);
     ErrorCode loadFont(const std::string& id, const std::string& path);
     ErrorCode loadSoundBuffer(const std::string& id, const std::string& path);
-
     Texture& getTexture(const std::string& id);
     Font& getFont(const std::string& id);
     SoundBuffer& getSoundBuffer(const std::string& id);
-
     bool hasTexture(const std::string& id) const;
     bool hasFont(const std::string& id) const;
 };
